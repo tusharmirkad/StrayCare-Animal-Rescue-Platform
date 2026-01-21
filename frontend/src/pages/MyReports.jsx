6306838f1cedef67089fa934b114090bdc66c18b
Tushar Mirkad <tusharmirkad@gmail.com>
2026-01-21 15:41:53 +0530

    Add empty line for better readability in MyReports

--- frontend/src/pages/MyReports.jsx
+++ frontend/src/pages/MyReports.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../components/navbar";
 import useApi from "../utils/api";
 
+
 const MyReports = () => {
   const api = useApi();
 
