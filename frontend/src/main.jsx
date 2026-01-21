1690b325ddcbbc3d8c9d88397d2a0d2c991e2c76
Tushar Mirkad <tusharmirkad@gmail.com>
2026-01-21 15:38:51 +0530

    Integrate ToastContainer for notification support
    Added ToastContainer for notifications in the app.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { ToastContainer } from "react-toastify";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -14,6 +15,16 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <App />
+       <ToastContainer
+      position="top-right"
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      pauseOnHover
+      draggable
+      theme="light"
+    />
     </ClerkProvider>
   </StrictMode>
 );
